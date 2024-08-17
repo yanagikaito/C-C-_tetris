8f05db3955a1d83a2eb16aa9bc1db955e0131a29
orutanakaito <voltgt777@gmail.com>
2024-08-17 23:42:44 +0900

    フレームの色はグレーで、ブロックを実際に操作する背景を白に描画してみました。

--- Main.cpp
+++ Main.cpp
@@ -8,7 +8,7 @@
 #define BLOCK_NUM_Y (20)
 
 // ブロックの1個1個の大きさ
-#define BLOCK_EDGE  (40)
+#define BLOCK_EDGE  (32)
 
 // 列挙体(ブロックタイプ)
 enum e_Color {
@@ -28,6 +28,36 @@ struct BLOCK {
     int y;
     int c;
 };
+
+// 色を決定する関数
+unsigned int Color(int c) {
+
+    // 色代入
+    switch (c) {
+        // 白
+    case e_Color::Col_No:
+        c = GetColor(255, 255, 255);
+        break;
+        // 赤
+    case e_Color::Col_Red:
+        c = GetColor(255, 0, 0);
+        break;
+        // 青
+    case e_Color::Col_Blue:
+        c = GetColor(0, 0, 255);
+        break;
+        // 緑
+    case e_Color::Col_Green:
+        c = GetColor(0, 0, 255);
+        break;
+        // 黄色
+    case e_Color::Col_Yellow:
+        c = GetColor(0, 0, 255);
+        break;
+    }
+
+    return c;
+}
 
 /*** Keyクラス ***/
 class
@@ -83,8 +113,45 @@ void Game_Cal() {
 }
 
 // ゲームの描画処理を行う関数
-void Game_Draw() {
+void Game_Draw(int y, int x, struct BLOCK(*p)) {
 
+    // フレームX
+    for (int x = 0; x < BLOCK_NUM_X + 2; x = x + 1) {
+        DrawBox(x * BLOCK_EDGE,
+            BLOCK_EDGE * BLOCK_NUM_Y,
+            BLOCK_EDGE * BLOCK_EDGE,
+            BLOCK_EDGE * BLOCK_NUM_Y + BLOCK_EDGE,
+            GetColor(100, 100, 100),
+            TRUE);
+    }
+
+    // フレームY
+    for (int x = 0; x < 2; x = x + 1) {
+        for (int y = 0; y < BLOCK_NUM_Y; y = y + 1) {
+            DrawBox(
+                x * (BLOCK_NUM_X + 1) * BLOCK_EDGE,
+                y * BLOCK_EDGE,
+                x * (BLOCK_NUM_X + 1) * BLOCK_EDGE + BLOCK_EDGE,
+                y * BLOCK_EDGE + BLOCK_EDGE,
+                GetColor(100, 100, 100),
+                TRUE);
+        }
+    }
+
+    // 描画用シフト分の座標
+    const int shi_x = BLOCK_EDGE;
+
+    // ブロック
+    for (int y = 0; y < BLOCK_NUM_Y; y = y + 1) {
+        for (int x = 0; x < BLOCK_NUM_X; x = x + 1) {
+            DrawBox(shi_x + p->x * BLOCK_EDGE + BLOCK_EDGE,
+                p->y * BLOCK_EDGE,
+                shi_x + p->x - BLOCK_EDGE,
+                p->y + BLOCK_EDGE,
+                Color(p->c),
+                TRUE);
+        }
+    }
 }
 
 // プログラムは WinMain から始まります
@@ -113,10 +180,11 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance,
     /*** Window Init ***/
     SetWindowText("tetris");
     SetWindowInitPosition(0, 0);
-    SetGraphMode(900, 900, 32);
+    SetGraphMode(900, 800, 32);
     SetBackgroundColor(0, 0, 0);				    // ウィンドウの背景色
     SetDrawScreen(DX_SCREEN_BACK);					// 描画先画面を裏画面にする
     SetAlwaysRunFlag(TRUE);							// ウインドウ非アクティブ状態でも処理を続行する
+
 
     Game_Ini(BLOCK_NUM_X, BLOCK_NUM_Y, p);
 
@@ -127,6 +195,8 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance,
         Key.GetKey() == 0 &&						// キーボード入力情報取得
         ProcessMessage() == 0)		                // ウインドウのメッセージを処理
     {
+        Game_Cal();
+        Game_Draw(BLOCK_NUM_X, BLOCK_NUM_Y, p);
     }
 
     DxLib_End();					                // ＤＸライブラリ使用の終了処理
